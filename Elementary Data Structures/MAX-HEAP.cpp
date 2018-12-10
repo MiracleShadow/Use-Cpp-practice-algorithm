ccbb9baf2dc0b71f2d1e1af53df1d8ac9273a3a3
MiracleShadow <1210768621@qq.com>
2018-12-10 22:25:26 +0800

    :+1:  append max-heap code

--- Elementary Data Structures/MAX-HEAP.cpp
+++ Elementary Data Structures/MAX-HEAP.cpp
@@ -16,34 +16,55 @@ const int RIGHT(int i) {
 
 class MAX_HEAP
 {
-    size_t heap_size;
-    size_t length;
+public:
+    size_t heap_size;   /* 堆元素个数，有效数据 */
+    size_t length;      /* 数组长度 */
     std::vector<int> data;
+    MAX_HEAP(std::vector<int> t);
 };
 
+MAX_HEAP::MAX_HEAP(std::vector<int> t) {
+    this->data = t;
+    this->length = t.size()-1;
+    // this->heap_size = t.size()-1;
+}
 
 /* 
 * 最大堆化，维护最大对性质的关键
 * 时间复杂度O(lgn)
 */
-void MAX_HEAPIFY(int i ) {
-
+void MAX_HEAPIFY(MAX_HEAP &heap, int i) {
+    int l = LEFT(i), r = RIGHT(i);
+    int largest = i;
+    if(l <= heap.heap_size && heap.data[l] > heap.data[i]) {
+        largest = l;
+    }
+    if(r <= heap.heap_size && heap.data[r] > heap.data[largest]) {
+        largest = r;
+    }
+    if(largest != i) {
+        std::swap(heap.data[i], heap.data[largest]);
+        MAX_HEAPIFY(heap, largest);
+    }
 }
 
 /*
 * 从无序的输入数据数组中构造一个最大堆
 * 时间复杂度O(n)
 */
-void BUILD_MAX_HEAP(MAX_HEAP A) {
-
+void BUILD_MAX_HEAP(MAX_HEAP &heap) {
+    heap.heap_size = heap.length;
+    for (int i = floor(heap.length / 2); i >= 1; --i) {
+        MAX_HEAPIFY(heap, i);
+    }
 }
 
 /*
 * 对一个数组进行原址排序
 * 复杂度O(nlgn)
 */
-void HEAPSORT() {
-
+void HEAPSORT(int *a) {
+    
 }
 
 void MAX_HEAP_INSERT() {
@@ -63,6 +84,25 @@ void HEAP_MAXIMUM() {
 }
 
 int main() {
-    
+    std::vector<int> t(1, 0);
+    int n, num;
+    std::cin >> n;
+    for (int i = 0; i < n; ++i) {
+        std::cin >> num;
+        t.push_back(num);
+    }
+    MAX_HEAP heap(t);
+    std::cout << "myheap.length=" << heap.length << std::endl;
+    for (int i = 1; i <= heap.length; ++i)
+    {
+        std::cout << heap.data[i] << " ";
+    }
+    std::cout << std::endl;
+    BUILD_MAX_HEAP(heap);
+    for (int i = 1; i <= heap.heap_size; ++i)
+    {
+        std::cout << heap.data[i] << " ";
+    }
+    std::cout << std::endl;
     return 0;
 }
