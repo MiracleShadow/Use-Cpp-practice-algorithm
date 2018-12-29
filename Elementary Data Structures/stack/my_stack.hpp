ae09afc5dd152b441f9be57e457e0652785e7849
MiracleShadow <1210768621@qq.com>
2018-12-29 23:57:50 +0800

    Create my_stack.hpp

---
+++ Elementary Data Structures/stack/my_stack.hpp
@@ -0,0 +1,10 @@
+#ifndef MY_STACK
+#define MY_STACK
+
+namespace miracle_shadow {
+       template <class T,
+                 class Container = deque<T> >
+       class stack;
+   }
+
+#endif  // MY_STACK
