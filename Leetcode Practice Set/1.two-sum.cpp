af56b860d12a142072990ac10ffe170a755a4797
MiracleShadow <1210768621@qq.com>
2019-02-05 22:12:01 +0800

    Create 1.two-sum.cpp

---
+++ Leetcode Practice Set/1.two-sum.cpp
@@ -0,0 +1,44 @@
+/*
+ * @lc app=leetcode.cn id=1 lang=cpp
+ *
+ * [1] 两数之和
+ *
+ * https://leetcode-cn.com/problems/two-sum/description/
+ *
+ * algorithms
+ * Easy (43.78%)
+ * Total Accepted:    219.4K
+ * Total Submissions: 501.1K
+ * Testcase Example:  '[2,7,11,15]\n9'
+ *
+ * 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
+ * 
+ * 你可以假设每种输入只会对应一个答案。但是，你不能重复利用这个数组中同样的元素。
+ * 
+ * 示例:
+ * 
+ * 给定 nums = [2, 7, 11, 15], target = 9
+ * 
+ * 因为 nums[0] + nums[1] = 2 + 7 = 9
+ * 所以返回 [0, 1]
+ * 
+ * 
+ */
+static const auto SpeedUp = []{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    return 0;
+}();
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> mp;
+        for(int i = 0; i < nums.size(); ++i) {
+            if(mp.find(target - nums[i]) != mp.end()) {
+                return {mp[target - nums[i]], i};
+            }
+            mp.insert(make_pair(nums[i], i));
+        }
+        return {};        
+    }
+};
